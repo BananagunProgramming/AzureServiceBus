b01af85035246a66015363a9dcaf8ce205f2a987
bananagun <bananagun@live.com>
2015-06-27 12:39:56 -0400

    Service bus demo

---
+++ ServiceBus/RecieveFromQueue/RecieveFromQueue/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ReceiveFromQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceBusRecieve();
+        }
+
+        private static void ServiceBusRecieve()
+        {
+            string qName = "76BusQueue";
+            string connection =
+                "Endpoint=sb://alewife.servicebus.windows.net/;SharedAccessKeyName=76BusReceiver;SharedAccessKey=/ot1r8HCUNnLScKABbOHharCzOBQqoNtvbxlfVKPyRQ=;TransportType=Amqp";
+
+            MessagingFactory factory = MessagingFactory.CreateFromConnectionString(connection);
+
+            QueueClient queue = factory.CreateQueueClient(qName);
+
+            var counter = 1;
+            while (true)
+            {
+                BrokeredMessage bm = queue.Receive();
+
+                if (bm != null)
+                {
+                    try
+                    {
+                        Console.WriteLine("MessageId {0}", bm.MessageId);
+                        Console.WriteLine(counter);
+                        Console.WriteLine(bm.GetBody<string>());
+                        bm.Complete();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        bm.Abandon();
+                    }
+                }
+                counter++;
+            }
+        }
+    }
+}
---
+++ ServiceBus/SendToQueue/SendToQueue/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.ServiceBus.Messaging;
+
+namespace SendToQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceBusSend();
+        }
+
+        private static void ServiceBusSend()
+        {
+            string qName = "76BusQueue";
+            string connection =
+                "Endpoint=sb://alewife.servicebus.windows.net/;SharedAccessKeyName=76BusSender;SharedAccessKey=o23NObgIFcYaSALAJZazrf9wTP/BXrahH8erpeoO4iY=;TransportType=Amqp";
+
+            MessagingFactory factory = MessagingFactory.CreateFromConnectionString(connection);
+
+            QueueClient queue = factory.CreateQueueClient(qName);
+
+            for (int i = 0; i < 100; i++)
+            {
+                string message = "Thurman Thomas is old -" + DateTime.UtcNow.Ticks;
+                BrokeredMessage bm = new BrokeredMessage(message);
+
+                queue.Send(bm);
+
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
